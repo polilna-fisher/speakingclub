8c0ad0090b9426a53e9de7269f8cd04edc5660cf
fisher <fisherpoly1995@gmail.com>
2024-07-31 22:11:50 +0200

    Change routes

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+export const routes = {
+        default: "/",
+        schedule: "/schedule",
+        premium: "/premium",
+        profile: "/profile",
+        help: "/help",
+        admin: "/admin",
+        notFound: "*"
+}
